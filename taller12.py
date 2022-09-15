8487ada9f7668e1aadb1205776c8c0021080485f
Jair Acevedo <110878217+kobbii3@users.noreply.github.com>
2022-09-15 07:52:07 -0500

    Update taller12.py

--- taller12.py
+++ taller12.py
@@ -1,23 +1,25 @@
 import math
 from math import factorial
 from math import e
+from decimal import Decimal
 
 ea = 0
 xi = 0.55
 h = 0.005
+getcontext().prec = 50
 
 iteraciones = 1
 potencia = 1
-valor = math.e**-xi
+valor = Decimal(math.e**-xi)
 signo = -1
 print("ORDEN 0:")
 print(valor)
 print("-------------------")
 for iteraciones in range(1,16):
-  valor_ant = valor
-  op = signo*(valor/factorial(iteraciones))*(pow(h,potencia))
-  valor += op
-  ea = abs((valor-valor_ant)/valor)*100
+  valor_ant = Decimal(valor)
+  op = signo*(Decimal(valor)/Decimal(factorial(iteraciones)))*(Decimal(pow(h,potencia)))
+  valor += Decimal(op)
+  ea = abs((Decimal(valor)-Decimal(valor_ant))/Decimal(valor))*100
   potencia += 1
   signo *= -1
   print("ORDEN", iteraciones , ":")
